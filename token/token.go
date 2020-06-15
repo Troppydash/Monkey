317b64d51be43851633580fd19e5856e0768d344
troppydash <troppydash.developer@gmail.com>
2020-06-16 11:36:52 +1200

    Initial Commit

---
+++ token/token.go
@@ -0,0 +1,39 @@
+package token
+
+// All Token Types
+const (
+	// Others
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	// Specials
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+// The Type of a Token
+type TokenType string
+
+// A Token
+type Token struct {
+	Type    TokenType
+	Literal string
+}
