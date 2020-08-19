a96b70cbe56b619f974b46de879c203030f177bf
Terry Qi <fortianrui4@gmail.com>
2020-08-19 20:31:43 +1200

    Added py Script

--- script/add_to_path.py
+++ script/add_to_path.py
@@ -1,1 +1,10 @@
-print("This works")
+import os
+import platform
+
+root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+plat = platform.system()
+print("Please enter the following in your respective terminal:")
+if plat == 'Linux' or plat == 'Darwin':
+    print(f"export MKYROOT={root}")
+elif plat == 'Windows':
+    print(f"set MKYROOT={root}")
